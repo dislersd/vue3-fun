9f8cd96029e00083cdee855803e5086ab2ec9a17
dislersd <dislersd@gmail.com>
2020-05-22 22:36:24 -0700

    init

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+    <HelloWorld msg="VUE 3 FTW" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
